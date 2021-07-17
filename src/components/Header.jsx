21850cf7a366364898082f18e3d010f0edd8acd0
muzeeali <64666168+muzeeali@users.noreply.github.com>
2021-07-18 03:41:13 +0500

    Create Header.jsx

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div>
+            <header>
+      <h1>
+        TODO APP
+      </h1>
+    </header>
+        </div>
+    );
+}
+
+export default Header;
