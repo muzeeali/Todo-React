460009a266ae0214961d871bfb4f3aa376e2cc68
muzeeali <64666168+muzeeali@users.noreply.github.com>
2021-08-02 02:58:36 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-
 import './App.css';
 import Header from './components/Header';
 import React, { useState } from 'react';
@@ -7,36 +6,58 @@ function App() {
 
 
 
-  const [iValue,setValue]=useState("");
-  const [item,setItem]=useState([]);
+  const [iValue, setValue] = useState("");
+  const [item, setItem] = useState([]);
 
-  const setVal=()=>{
-    setItem(()=>{
-      return( [...item,iValue]);
+  const setVal = () => {
+    setItem(() => {
+      return ([...item, iValue]);
+      
     });
-    // console.log(iValue)
+    setValue("");
   };
+  const deleteItem = (i) => {
+    setItem((old) => {
+      return old.filter((item,id)=>{
+return i !== id;
+      });
+      
+    }); 
+   }
+
+ const deleteAll = () => {
+ setItem(()=>{
+   return []
+ });
+}
+
+const edit = (index) => {
+  const newVal = prompt("Enter");
+
+  setItem(() => {
+    item[index]=newVal
+    return ([...item]);
+    
+  });
+}
 
   return (
-  
-    <div >
-  
-<Header/>
-
-       <form>
-         <input  value={iValue}  onChange={e => setValue(e.target.value)} type="text"/>
-      
-       </form>
-       <button onClick={setVal} >ADD</button>
-<ul>
-  
-     {item.map((setItem,i) =>
-    {
-return <li key={i} >{setItem}</li>
-    })}
-  
-</ul>
-
+    <div className='main'>
+      <Header />
+      <form>
+        <input value={iValue} onChange={e => setValue(e.target.value)} type="text" />
+      </form>
+      <button onClick={setVal} >ADD</button>
+      <button onClick={deleteAll}>DELETE ALL</button>
+      <div  id='ui'>
+      <ul>
+        {item.map((val, i) => {
+          return <li key={i} >  {val} <span className='btn'><button   onClick={()=>deleteItem(i)}>DELETE</button>
+          <button   onClick={()=>edit(i)}>UPDATE</button></span> 
+          </li>
+        })}
+      </ul>
+      </div>
     </div>
   );
 }
